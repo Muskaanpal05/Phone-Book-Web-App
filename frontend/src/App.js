41761144f422c2f24f3bbf8d4adcae9f48b41cbd
Muskaanpal05 <palmuskaan5@gmail.com>
2020-05-31 16:45:38 +0530

    Implemented css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Main from './components/Main.js';
-// import './App.css';
+import './App.css';
 
 
 class App extends Component {
   render(){
     return (
-      <div>
+      <div className="App">
         <Main />
       </div>
     );
